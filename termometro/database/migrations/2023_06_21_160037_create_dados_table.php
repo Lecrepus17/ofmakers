710f3fabc3ce7ef69c1cd49a0fde7bae6a1526a2
Pedro Sperotto <114098634+Lecrepus17@users.noreply.github.com>
2023-06-21 13:14:57 -0300

    controller e migration

--- termometro/database/migrations/2023_06_21_160037_create_dados_table.php
+++ termometro/database/migrations/2023_06_21_160037_create_dados_table.php
@@ -4,24 +4,31 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateDadosTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('dados', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->float('temperatura');
+            $table->integer('umidade');
+            $table->dateTime('tempo');
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('dados');
     }
-};
+}
