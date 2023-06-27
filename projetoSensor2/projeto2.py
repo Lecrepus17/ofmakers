c2984bc32588a98a46b302b55c91663956ff1714
Pedro Sperotto <114098634+Lecrepus17@users.noreply.github.com>
2023-06-27 10:51:47 -0300

    Update projeto2.py

--- projetoSensor2/projeto2.py
+++ projetoSensor2/projeto2.py
@@ -3,14 +3,17 @@ import time
 import board
 import adafruit_dht
 import mysql.connector
+import os
 # Initial the dht device, with data pin connected to:
 # dhtDevice = adafruit_dht.DHT11(board.D4)
 # you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
 # This may be necessary on a Linux single board computer like the Raspberry Pi,
 # but it will not work in CircuitPython.
 
-dhtDevice = adafruit_dht.DHT11(board.D4, use_pulseio=False)
+dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
 day_old = 0
+year_old = 0
+month_old = 0
 # Estabelecendo uma conexão com o banco de dados MySQL
 db = mysql.connector.connect(
 host='localhost',
@@ -23,12 +26,32 @@ while True:
         
         # Print the values to the serial port
 	# Nomeando as temperaturas
+
+        
+        # Print the values to the serial port
+	# Nomeando as temperaturas
         dateNow = datetime.datetime.now()
-        data_formatada = dateNow.strftime("%Y-%m-%d %H:%M:%S")
         temperature_c = dhtDevice.temperature
         humidity = dhtDevice.humidity
         now=time.localtime()
+        year = now.tm_year
+        month_str = (str(now.tm_mon).rjust(2, '0'))
+        month = now.tm_mon
+        # Nomeando os meses por escrito
+        monthName = dateNow.strftime("%b")
+	# Nomeando dia
+        day_str = (str(now.tm_mday).rjust(2, '0'))
         day = now.tm_mday
+	# Nomeando hora
+        hour_str = (str(now.tm_hour).rjust(2, '0'))
+        hour = now.tm_hour
+        # Nomeando minuto
+        min_str = (str(now.tm_min).rjust(2, '0'))
+        minute = now.tm_min
+        # Nomeando segundo
+        sec_str = (str(now.tm_sec).rjust(2, '0'))
+        second = now.tm_sec
+        data_formatada = dateNow.strftime("%Y-%m-%d %H:%M:%S")
 	# Atualiza o dia
         if day != day_old:
           day_old = day
@@ -37,12 +60,10 @@ while True:
             if year != year_old:
               year_old = year
               foldername =  ("{}_".format( year ))
-              if os.path.exists(foldername):
-              else:
+              if not os.path.exists(foldername):
                 os.mkdir(foldername)
             foldername_m = ("{}/{}_". format( foldername, month_str ))
-            if os.path.exists(foldername_m):
-            else:
+            if not os.path.exists(foldername_m):
               os.mkdir(foldername_m)
           filename = ("{}/{}{}{}_dados.txt".format( foldername_m, year, monthName, day_str))
           # fechando conexão com arquivo
@@ -58,6 +79,14 @@ while True:
           f.write("Temperatura: {:.1f} ºC  Humidade: {} %  Horário: {}:{}:{}  Data: {}/{}/{}\n".format(
                 temperature_c, humidity, hour_str, min_str, sec_str, day_str, month_str, year
             ))
+
+          db = mysql.connector.connect(
+          host='localhost',
+          database='termometro',
+          user='user',
+          password='123'
+          )
+
           # Criando um cursor para executar consultas SQL
           cursor = db.cursor()
 
@@ -79,5 +108,4 @@ while True:
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
-        
 
