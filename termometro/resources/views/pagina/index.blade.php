dbae8737cfabe70d358588728503ac5f175a4d27
Gustavo Rajab <gusaodbr@gmail.com>
2023-07-12 13:52:48 -0300

    Update index.blade.php

--- termometro/resources/views/pagina/index.blade.php
+++ termometro/resources/views/pagina/index.blade.php
@@ -345,18 +345,7 @@
 
               <div class="card info-card customers-card">
 
-                <div class="filter">
-                  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <li class="dropdown-header text-start">
-                      <h6>Filter</h6>
-                    </li>
 
-                    <li><a class="dropdown-item" href="#">Maior</a></li>
-                    <li><a class="dropdown-item" href="#">Menor</a></li>
-                    <li><a class="dropdown-item" href="#">Média</a></li>
-                  </ul>
-                </div>
 
                 <div class="card-body">
                   <h5 class="card-title">Temperatura atual <span></span></h5>
